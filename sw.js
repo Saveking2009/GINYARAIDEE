1070e70649b864964c2dce8087c1e1cd804a11ba
Saveking2009 <161796554+Saveking2009@users.noreply.github.com>
2026-05-15 21:24:24 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nong-jaidee-v1';
+const CACHE_NAME = 'nong-jaidee-v2';
 const ASSETS = [
   'index.html',
   'style.css',
@@ -9,6 +9,16 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
